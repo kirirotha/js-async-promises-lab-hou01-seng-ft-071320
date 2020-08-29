da42acc140483f1d056b4b4d73156f5bf3e1fe29
kirirotha <kirirotha@gmail.com>
2020-08-29 18:04:29 -0500

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,53 @@ const questions = [
 ]
 
 let question;
+
+
+const postQuestions = (questions) => {
+  questions.forEach(function(a_question){
+      appendQuestion(a_question)
+  })
+}
+
+
+const appendQuestion = (aQuestion) => {
+  const questionIn = aQuestion.questionText
+  document.querySelector('.question-container').innerHTML = questionIn
+}
+
+const askQuestionThen = (time) => {
+  questions.forEach(function(a_question){
+    question = a_question
+  })
+  return new Promise(() =>{
+    })
+}
+
+const removeQuestion = () => {
+  document.querySelector('.question-container').innerText = ''
+}
+
+const askQuestionThenRemoveQuestion = (time) => {
+  question = questions[0]
+  appendQuestion(question)
+  return new Promise(() =>{
+    setTimeout(function(){
+      removeQuestion()
+    }, time)
+  })
+}
+
+const trueAndFalseButtons = () => {
+  return document.querySelectorAll('div.btn')
+}
+
+const toggleTrueAndFalseButtons = () => {
+  document.querySelector('div.btn.red').classList.toggle("hide")
+  document.querySelector('div.btn.green').classList.toggle("hide")
+}
+
+const displayQuestionOnClick = () =>{
+  document.querySelector('.waves-effect').addEventListener('click', appendQuestion(question))
+  toggleTrueAndFalseButtons()
+}
+
